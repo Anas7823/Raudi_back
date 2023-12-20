8a2861481bf41d2471e0c8b1fbea921bf1f8b501
Anas7823 <anaselkhiat78@gmail.com>
2023-12-20 17:03:59 +0100

    route model

--- controllers/modeleController.js
+++ controllers/modeleController.js
@@ -13,3 +13,28 @@ exports.getAllModele = async (req, res) => {
         res.status(400).json({message: err.message});
     }
 }
+
+exports.getOneModele = async (req, res) => {
+    try {
+        const modeleFound = await modele.findOne({
+            where: {
+                id_modele: req.params.id
+            }
+        });
+        if (modeleFound == null) {
+            return res.status(400).json({message: "modele introuvable"});
+        }
+        res.status(200).json(modeleFound);
+    } catch (err) {
+        res.status(400).json({message: err.message});
+    }
+}
+
+exports.createModele = async (req, res) => {
+    try {
+        const modeleCreated = await modele.create(req.body);
+        res.status(201).json(modeleCreated);
+    } catch (err) {
+        res.status(400).json({message: err.message});
+    }
+}
--- routes/modeleRoute.js
+++ routes/modeleRoute.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const modeleController = require('../controllers/modeleController');
 
 router.get('/modeles', modeleController.getAllModele);
+router.get('/modeles/:id', modeleController.getOneModele);
+router.post('/modeles', modeleController.createModele);
 
 module.exports = router;
