14bfd5073500d421c2fe2f493b4401570dc1c5ca
Anas7823 <anaselkhiat78@gmail.com>
2023-12-20 17:32:37 +0100

    route options

--- controllers/optionController.js
+++ controllers/optionController.js
@@ -13,3 +13,44 @@ exports.getAllOption = async (req, res) => {
         res.status(400).json({message: err.message});
     }
 }
+
+exports.getOneOption = async (req, res) => {
+    try {
+        const optionFound = await option.findOne({
+            where: {
+                id_option: req.params.id
+            }
+        });
+        if (optionFound == null) {
+            return res.status(400).json({message: "option introuvable"});
+        }
+        res.status(200).json(optionFound);
+    } catch (err) {
+        res.status(400).json({message: err.message});
+    }
+}
+
+exports.addOptionToModele = async (req, res) => {
+    try {
+        const modeleFound = await modele.findOne({
+            where: {
+                id_modele: req.params.id
+            }
+        });
+        if (modeleFound == null) {
+            return res.status(400).json({message: "modele introuvable"});
+        }
+        const optionFound = await option.findOne({
+            where: {
+                id_option: req.body.id_option
+            }
+        });
+        if (optionFound == null) {
+            return res.status(400).json({message: "option introuvable"});
+        }
+        await modeleFound.addOption(optionFound);
+        res.status(201).json({message: "option ajouté au modele"});
+    } catch (err) {
+        res.status(400).json({message: err.message});
+    }
+}
--- routes/optionRoute.js
+++ routes/optionRoute.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const optionController = require('../controllers/optionController');
 
 router.get('/options', optionController.getAllOption);
+router.get('/options/:id', optionController.getOneOption);
+router.post('/options/:id', optionController.addOptionToModele);
 
 module.exports = router;
